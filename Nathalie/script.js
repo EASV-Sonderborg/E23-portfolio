3d60ca336717131b26e44fa840b68e3182e4d116
Nathalie Autzen <nathalie@autzen.eu>
2023-09-13 14:27:35 +0200

    Lidt forskelligt
    Produktion til portfolio, mangle kun tekst. Ændret profil billede tilbage, da jeg ikke kunne få clip-path og dias til at virke sammentidig.

---
+++ Nathalie/script.js
@@ -0,0 +1,23 @@
+/** Ændre profil billede */
+// Få fat i alle elementer med klassen "changingImage"
+const changingImages = document.querySelectorAll('.profile__images');
+
+// En liste med de forskellige billedkilder, du vil skifte mellem
+const imageSources = ['images/Profile.png', 'images/full_profile.png', 'images/profile-picture.png'];
+
+// En variabel til at spore det aktuelle billede for hver klasse
+const currentImageIndexes = Array(changingImages.length).fill(0);
+
+// Funktion til at ændre billedet for hvert element med klassen "changingImage"
+function changeImages() {
+  changingImages.forEach((image, index) => {
+    // Opdater src-attributten med den næste kilde i listen
+    image.src = imageSources[currentImageIndexes[index]];
+
+    // Øg indekset, og sørg for, at det forbliver inden for listen
+    currentImageIndexes[index] = (currentImageIndexes[index] + 1) % imageSources.length;
+  });
+}
+
+// Sæt intervallet til at ændre billederne hvert 4. sekund (4000 millisekunder)
+setInterval(changeImages, 4000);
